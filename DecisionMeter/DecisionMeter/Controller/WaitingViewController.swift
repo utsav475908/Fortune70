5a4f1f2293121b379e2240338546faf0373ee8af
Avishek Sinha <avisheksinha@avisheks-imac.local>
2017-09-20 15:11:08 +0530

    UI Fixing in progress

--- DecisionMeter/DecisionMeter/Controller/WaitingViewController.swift
+++ DecisionMeter/DecisionMeter/Controller/WaitingViewController.swift
@@ -20,13 +20,14 @@ struct Decide {
 
 class WaitingViewController: UIViewController {
     struct TimerConstants {
-        static let kTimerConstant:Int = 60
+        static let kTimerConstant:Int = 10
     }
     
     @IBOutlet weak var startButton: CustomButton!
     @IBOutlet weak var pauseButton:CustomButton!
     @IBOutlet weak var timerLabel:UILabel!
     
+    @IBOutlet weak var resetButton: CustomButton!
     var seconds = TimerConstants.kTimerConstant
     var timer = Timer()
     var navigationKey:String?
@@ -34,11 +35,22 @@ class WaitingViewController: UIViewController {
     var resumeTapped = false;
     var errorCode404:Int?
     
+    @IBOutlet weak var submitButton: CustomButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         pauseButton.isEnabled = false
+        startButton.alpha = 0.0
+        pauseButton.alpha = 0.0
+        timerLabel.alpha = 0.0
+        resetButton.alpha = 0.0
+        submitButton.alpha = 0.0
+        runTimer()
         addNotificationForDownloadDataFromInternet()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        timer.invalidate()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -46,7 +58,7 @@ class WaitingViewController: UIViewController {
     }
     
     func runTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         isTimerRunning = true
         pauseButton.isEnabled = true
     }
@@ -112,18 +124,20 @@ class WaitingViewController: UIViewController {
         pauseButton.isEnabled = false
         startButton.isEnabled = true 
     }
-
+    
     
     func updateTimer() {
-        if seconds < 1 {
-            timer.invalidate()
-            // send alert to indicate time up
-        }else {
-            seconds -= 1
-            timerLabel.text = timeString(time: TimeInterval(seconds))
-            timerLabel.text = String(seconds)
-        }
-        
+//        if seconds < 1 {
+//            timer.invalidate()
+//            // send alert to indicate time up
+//        }else {
+//            seconds -= 1
+//            timerLabel.text = timeString(time: TimeInterval(seconds))
+//            timerLabel.text = String(seconds)
+//            doThePolling()
+//            // write the code
+//        }
+        doThePolling()
     }
     
     func timeString(time:TimeInterval) -> String {
@@ -133,23 +147,25 @@ class WaitingViewController: UIViewController {
         return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
     }
     
-    
-    
-    
-    
-    @IBAction func onSubmitButtonPressed(_ sender: CustomButton) {
+    func doThePolling () {
         let defaults = UserDefaults.standard
         let token = defaults.value(forKey: "session") as? String
         print(token!)
         let theQuestionId = defaults.value(forKey: "questionId")!
         print(theQuestionId)
-        //this questionId is the deciding factor 
+        //this questionId is the deciding factor
         
-      //Http.httpRequest(session: token, viewController: self)
+        //Http.httpRequest(session: token, viewController: self)
         Http.httpRequest(session: token!, viewController: self, searchNext: true)
+    }
+    
+    
+    
+    @IBAction func onSubmitButtonPressed(_ sender: CustomButton) {
+        doThePolling()
         
         
-//        present(vc!, animated: true, completion: nil);
+        //        present(vc!, animated: true, completion: nil);
     }
     
     func hasTheQuestionIdChanged(notification:NSNotification) {
@@ -160,14 +176,16 @@ class WaitingViewController: UIViewController {
             //dictsValue["status"]!.int64Value == 404
             if  arrayElementsGot["status"]?.int64Value == 404 {
                 print("Raise error")
+                /*
                 DispatchQueue.main.async {
                     [weak self] value in
                     let ac =  UIAlertController.alertWithTitle(title: "NO Question", message: "NO Question", buttonTitle: "NO Question")
-//                    self?.tokenTextField.text = ""
+                    //                    self?.tokenTextField.text = ""
                     // the question has not changed , so stay upon this page or throw error.
                     self?.present(ac, animated: true, completion: nil)
                     
                 }
+                  */
             }
             
             let defaults = UserDefaults.standard
@@ -185,11 +203,11 @@ class WaitingViewController: UIViewController {
                 defaults.synchronize()
             }
             
-                //            guard arrayElementsGot["questionType"] as! String != "SINGLE_OPTION", arrayElementsGot["questionType"] as! String != "MULTIPLE_CHOICE"
-                //                else { return }
+            //            guard arrayElementsGot["questionType"] as! String != "SINGLE_OPTION", arrayElementsGot["questionType"] as! String != "MULTIPLE_CHOICE"
+            //                else { return }
             
             if let _ = arrayElementsGot["questionType"] {
-            
+                
                 if  arrayElementsGot["questionType"] as! String == "SINGLE_OPTION" {
                     defaults.set(arrayElementsGot["options"], forKey: "options")
                     defaults.synchronize()
@@ -201,14 +219,14 @@ class WaitingViewController: UIViewController {
                 }
             }
             
-          // nav key
+            // nav key
             navigationKey = arrayElementsGot["questionType"] as? String
             
-                
+            
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let returnVC : UIViewController
             switch navigationKey {
-           
+                
             case "MULTIPLE_CHOICE"?:
                 returnVC =  storyboard.instantiateViewController(withIdentifier: DecisionConstants.multiple) as! MultipleChoiceViewController
                 
@@ -226,46 +244,50 @@ class WaitingViewController: UIViewController {
             }
             
             
-        //
-//        print(" i am the JURY")
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//        let vc = storyboard.instantiateViewController(withIdentifier: "Single") as? SingleChoiceViewController
-        DispatchQueue.main.async {
-            // memory management kumar utsav vvimportant  
-            //self.present(vc!, animated: true, completion: nil);
-            self.presentTheViewController(viewController: returnVC)
+            //
+            //        print(" i am the JURY")
+            //        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            //        let vc = storyboard.instantiateViewController(withIdentifier: "Single") as? SingleChoiceViewController
+            DispatchQueue.main.async {
+                // memory management kumar utsav vvimportant
+                //self.present(vc!, animated: true, completion: nil);
+                self.presentTheViewController(viewController: returnVC)
+            }
+            
         }
         
+        
+    }
+    func presentTheViewController(viewController:UIViewController){
+        if viewController is WaitingViewController {
+            // do nothing
+        }else {
+        self.present(viewController, animated: true, completion: nil);
+        }
     }
     
     
-}
-        func presentTheViewController(viewController:UIViewController){
-            self.present(viewController, animated: true, completion: nil);
-        }
-        
-        
-//        func instantiateTheViewController(withIdentifier identifier:String,toNavigate navigationKey:String) -> UIViewController {
-//            //Decide.single
-//            // identifiers are like single multiple go with Decisionconstants.
-//            let returnVC : UIViewController
-//
-//            switch navigationKey {
-//            case "MULTIPLE_CHOICE":
-//              returnVC =  storyboard?.instantiateViewController(withIdentifier: DecisionConstants.multiple)
-//            case "SINGLE_OPTION":
-//              returnVC =   storyboard?.instantiateViewController(withIdentifier: DecisionConstants.single)
-//            case "RATING":
-//              returnVC =   storyboard?.instantiateViewController(withIdentifier: DecisionConstants.slider)
-//            case "RANGE":
-//               returnVC = storyboard?.instantiateViewController(withIdentifier: DecisionConstants.range)
-//
-//            default:
-//              returnVC =  storyboard?.instantiateViewController(withIdentifier: DecisionConstants.range)
-//            }
-//            return returnVC
-//
-//        }
+    //        func instantiateTheViewController(withIdentifier identifier:String,toNavigate navigationKey:String) -> UIViewController {
+    //            //Decide.single
+    //            // identifiers are like single multiple go with Decisionconstants.
+    //            let returnVC : UIViewController
+    //
+    //            switch navigationKey {
+    //            case "MULTIPLE_CHOICE":
+    //              returnVC =  storyboard?.instantiateViewController(withIdentifier: DecisionConstants.multiple)
+    //            case "SINGLE_OPTION":
+    //              returnVC =   storyboard?.instantiateViewController(withIdentifier: DecisionConstants.single)
+    //            case "RATING":
+    //              returnVC =   storyboard?.instantiateViewController(withIdentifier: DecisionConstants.slider)
+    //            case "RANGE":
+    //               returnVC = storyboard?.instantiateViewController(withIdentifier: DecisionConstants.range)
+    //
+    //            default:
+    //              returnVC =  storyboard?.instantiateViewController(withIdentifier: DecisionConstants.range)
+    //            }
+    //            return returnVC
+    //
+    //        }
     
     
     
